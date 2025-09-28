204ee2a8cd8d82a31ec8fb686700cbdc02753900
AsaV67 <asabvictoria@gmail.com>
2025-09-28 01:26:53 -0500

    stuff

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 var speed = 70
 func _physics_process(delta):
 	var direction=(terget.position-position). normalized()
-	velocity = direction * speed
-	look_at(terget.position)
+	var distance=(terget.position-position). length()
+	if distance < 75  and distance > 5:
+		velocity = direction * speed
+		#look_at(terget.position)
+	else : 
+		velocity = Vector2.ZERO
+
+	# Handle gravity
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
 	move_and_slide()
